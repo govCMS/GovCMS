1bfb204332a8f01add2d366c941cd7bef134f9eb
ruwanl <ruwan.lk@gmail.com>
2019-10-08 13:40:18 +1100

    Update settings.php

--- .tugboat/settings.php
+++ .tugboat/settings.php
@@ -39,6 +39,10 @@ if (PHP_SAPI === 'cli') {
   ini_set('memory_limit', '256M');
 }
 
+if (drupal_is_cli()) { 
+  ini_set('memory_limit', '512M');
+}
+
 $settings['trusted_host_patterns'] = ['.*'];
 
 $config['system.logging']['error_level'] = 'verbose';
