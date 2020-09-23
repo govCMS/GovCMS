168cbdaffd2433b3dd7563a779f66e531017bde2
Pandaski <pandaski@outlook.com.au>
2020-09-23 10:42:51 +1000

    Update docker namespace

--- .docker/push.sh
+++ .docker/push.sh
@@ -4,14 +4,14 @@
 #
 
 # Namespace for the image.
-DOCKERHUB_NAMESPACE=${DOCKERHUB_NAMESPACE:-govcmspoc}
+DOCKERHUB_NAMESPACE=${DOCKERHUB_NAMESPACE:-govcms}
 # Docker image version tag.
 IMAGE_VERSION_TAG=${IMAGE_VERSION_TAG:-}
 # Docker image tag prefix to be stripped from tag. Use " " (space) value to
 # prevent stripping of the version.
 IMAGE_VERSION_TAG_PREFIX=${IMAGE_VERSION_TAG_PREFIX:-8.x-}
 # Docker image edge tag.
-IMAGE_TAG_EDGE=${IMAGE_TAG_EDGE:-beta}
+IMAGE_TAG_EDGE=${IMAGE_TAG_EDGE:-2.x-beta}
 # Flag to force image build.
 FORCE_IMAGE_BUILD=${FORCE_IMAGE_BUILD:-}
 # Path prefix to Dockerfiles extension that is used as a name of the service.
--- .docker/release.sh
+++ .docker/release.sh
@@ -4,9 +4,9 @@
 #
 
 # Namespace for the image.
-DOCKERHUB_NAMESPACE=${DOCKERHUB_NAMESPACE:-govcmspoc}
+DOCKERHUB_NAMESPACE=${DOCKERHUB_NAMESPACE:-govcms}
 # Docker image edge tag.
-IMAGE_TAG_EDGE=${IMAGE_TAG_EDGE:-beta}
+IMAGE_TAG_EDGE=${IMAGE_TAG_EDGE:-2.x-beta}
 
 # Path prefix to Dockerfiles extension that is used as a name of the service.
 FILE_EXTENSION_PREFIX=${FILE_EXTENSION_PREFIX:-.docker/Dockerfile.}
@@ -16,8 +16,8 @@ for file in $(echo $FILE_EXTENSION_PREFIX"*"); do
 
     echo "==> Releasing \"$service\" image for service \"$DOCKERHUB_NAMESPACE/$service\""
     docker pull $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
-    docker tag $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE $DOCKERHUB_NAMESPACE/$service:latest
+    docker tag $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE $DOCKERHUB_NAMESPACE/$service:2.x-latest
 
-    echo "==> Tagging and pushing \"$service\" image to $DOCKERHUB_NAMESPACE/$service:latest"
-    docker push $DOCKERHUB_NAMESPACE/$service:latest
+    echo "==> Tagging and pushing \"$service\" image to $DOCKERHUB_NAMESPACE/$service:2.x-latest"
+    docker push $DOCKERHUB_NAMESPACE/$service:2.x-latest
 done
