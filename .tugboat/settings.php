12ceb04ca0c926419a60513a4b3a251b6c36d9a2
Pandaski <pandaski@outlook.com.au>
2020-09-30 10:09:09 +1000

    Cleanup the default tugboat settings.php file

--- .tugboat/settings.php
+++ .tugboat/settings.php
@@ -5,27 +5,11 @@
  *
  * Most settings based on Drupal defaults.
  * Comments removed for brevity.
+ *
  * @see https://git.drupalcode.org/project/drupal/blob/8.8.x/sites/default/default.settings.php
  */
 
-$databases = [];
-
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => 'sites/default/files/sync',
-);
-
-$settings['hash_salt'] = 'djfu983roawei90tk3;mva9023i9523';
-
-$settings['update_free_access'] = FALSE;
-
-$settings['file_scan_ignore_directories'] = [
-  'node_modules',
-  'bower_components',
-];
-
-$settings['entity_update_batch_size'] = 50;
-
-$databases['default']['default'] = array (
+$databases['default']['default'] = [
   'database' => 'tugboat',
   'username' => 'tugboat',
   'password' => 'tugboat',
@@ -34,14 +18,27 @@ $databases['default']['default'] = array (
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
-);
+];
+
+$config_directories = [
+  CONFIG_SYNC_DIRECTORY => 'sites/default/files/sync',
+];
+
+$settings['entity_update_batch_size'] = 50;
+$settings['trusted_host_patterns'] = ['.*'];
+$settings['hash_salt'] = 'djfu983roawei90tk3;mva9023i9523';
+$settings['update_free_access'] = FALSE;
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+
 if (PHP_SAPI === 'cli') {
   ini_set('memory_limit', '256M');
 }
 
-$settings['trusted_host_patterns'] = ['.*'];
-
+// Debugging.
 $config['system.logging']['error_level'] = 'verbose';
-error_reporting(E_COMPILE_ERROR|E_RECOVERABLE_ERROR|E_ERROR|E_CORE_ERROR);
+error_reporting(E_COMPILE_ERROR | E_RECOVERABLE_ERROR | E_ERROR | E_CORE_ERROR);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
