531222e6f1600723788542112eb00a81b273fbbe
Joseph Zhao <pandaski@outlook.com.au>
2020-12-10 21:21:23 +1100

    Test login process

--- tests/cy/cypress/integration/pages/user.spec.js
+++ tests/cy/cypress/integration/pages/user.spec.js
@@ -5,4 +5,7 @@ describe('Check user page', function () {
     it("Check login form should be visible for anonymous user", function () {
         cy.get('.user-login-form').should('be.visible')
     })
+    it("Test login", function () {
+        cy.login();
+    })
 })
