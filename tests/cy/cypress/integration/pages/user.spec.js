5f90b50f11cb2d4f5b3f510b5a938fdeb7dcebb2
Joseph Zhao <pandaski@outlook.com.au>
2020-12-10 21:14:22 +1100

    Add a placeholder for user page test

---
+++ tests/cy/cypress/integration/pages/user.spec.js
@@ -0,0 +1,8 @@
+describe('Check user page', function () {
+    before(() => {
+        cy.visit("user")
+    })
+    it("Check login form should be visible for anonymous user", function () {
+        cy.get('.user-login-form').should('be.visible')
+    })
+})
