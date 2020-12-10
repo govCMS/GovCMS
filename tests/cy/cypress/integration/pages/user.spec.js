faa30f2eba3860c343c22c97756e5cc642f9605c
Joseph Zhao <pandaski@outlook.com.au>
2020-12-10 21:27:06 +1100

    Add video as the default value

--- tests/cy/cypress/integration/pages/user.spec.js
+++ tests/cy/cypress/integration/pages/user.spec.js
@@ -2,9 +2,11 @@ describe('Check user page', function () {
     before(() => {
         cy.visit("user")
     })
+
     it("Check login form should be visible for anonymous user", function () {
         cy.get('.user-login-form').should('be.visible')
     })
+
     it("Test login", function () {
         cy.login();
     })
