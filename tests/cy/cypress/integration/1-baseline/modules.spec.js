72a6f561b96e99c785a4b9c1833c3cfea0684399
Pandaski <pandaski@outlook.com.au>
2022-02-02 21:09:59 +1100

    [GOVCMSD9-603] Clean up the test files

--- tests/cy/cypress/integration/1-baseline/modules.spec.js
+++ tests/cy/cypress/integration/1-baseline/modules.spec.js
@@ -1,14 +1,10 @@
 import packages from '../../../../../composer.json'
 
 let modules = Object.entries(packages['require']).filter((str) => {
-    return str[0].indexOf('drupal/') !== -1
-        && str[1].indexOf('^') === -1
-        && str[0].indexOf('drupal/core-recommended') === -1
-        && str[0].indexOf('theme') === -1
-        && str[0].indexOf('update_notifications_disable') === -1
+    return str[1].indexOf('^') === -1
 })
 
-describe('Cross check module versions', () => {
+describe('Cross check module and library versions', () => {
     modules.forEach((module, i) => {
         it(`${module}`, () => {
             cy.composerCommand('show ' + module[0] + ' | grep versions').its('stdout').should('contain', module[1])
