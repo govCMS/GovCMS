6c3adce81d80dbc0369203b12db9f12d4c5e81b8
Mingsong Hu <mingsonghu@Song.home>
2022-08-10 15:36:27 +1000

    [GOVCMSD9-883] Lock Security.txt in GovCMS

---
+++ modules/custom/core/govcms_security/src/GovcmsSecurityInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\govcms_security;
+
+/**
+ * Provides an interface for GovCMS secruity constants.
+ */
+interface GovcmsSecurityInterface {
+  /**
+   * Role ID for GovCMS Site Administrator role.
+   */
+  const GOVCMS_SITE_ADMIN_ROLE = 'govcms_site_administrator';
+}
