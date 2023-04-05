ea2d917c21fe91e6c2d8aed55dadfc2c714fac37
Pandaski <pandaski@outlook.com.au>
2023-04-05 19:40:39 +1000

    Update experimental post update hooks for migration tests.

--- govcms.post_update.php
+++ govcms.post_update.php
@@ -22,3 +22,47 @@ function govcms_post_update_replace_block_node_type_condition() {
     }
   }
 }
+
+/**
+ * Replaces the CKEditor to 5 from 4.
+ */
+function govcms_post_update_replace_ckeditor5() {
+  $config_factory = \Drupal::configFactory();
+  foreach ($config_factory->listAll('editor.editor.') as $editor_config_name) {
+    $editor = $config_factory->getEditable($editor_config_name);
+
+    if ($editor->get('editor') == 'ckeditor') {
+      // Set ckeditor5 as a dependency module.
+      $editor->set('dependencies.module', ['ckeditor5']);
+      // Set the editor to ckeditor5.
+      $editor->set('editor', 'ckeditor5');
+      // Set the default settings for ckeditor5.
+      $settings = [
+        // Add any additional settings as needed.
+      ];
+      $editor->set('settings', $settings);
+      // Save the updated configuration.
+      $editor->save();
+    }
+  }
+}
+
+/**
+ * Removes the entity_embed plugin from text filters.
+ */
+function govcms_post_update_remove_entity_embed() {
+  $plugin_id = 'entity_embed';
+  $config_factory = \Drupal::configFactory();
+  foreach ($config_factory->listAll('filter.format.') as $filter_config_name) {
+    $filter = $config_factory->getEditable($filter_config_name);
+
+    if ($filters = $filter->get('filters')) {
+      if (isset($filters[$plugin_id])) {
+        // Remove the filter plugin.
+        unset($filters[$plugin_id]);
+        $filter->set('filters', $filters);
+        $filter->save();
+      }
+    }
+  }
+}
