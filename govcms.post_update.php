ccdde1acc5e9d60d5845820f773ab25f12e700ca
Pandaski <pandaski@outlook.com.au>
2023-05-18 17:43:53 +1000

    Updates the condition for the node type in the pathauto module.

--- govcms.post_update.php
+++ govcms.post_update.php
@@ -42,6 +42,27 @@ function govcms_post_update_replace_context_node_type_condition() {
 }
 
 /**
+ * Updates the condition for the node type in the pathauto module.
+ */
+function govcms_post_update_replace_pathauto_node_type_condition() {
+  $config_factory = \Drupal::configFactory();
+  foreach ($config_factory->listAll('pathauto.pattern.') as $pattern_config_name) {
+    $pattern_config = $config_factory->getEditable($pattern_config_name);
+
+    if ($pattern_config->get('type') === 'canonical_entities:node') {
+      $selection_criteria = $pattern_config->get('selection_criteria');
+      foreach ($selection_criteria as $uuid => $condition) {
+        if ($condition['id'] === 'node_type') {
+          $pattern_config->set("selection_criteria.$uuid.id", 'entity_bundle:node');
+          $pattern_config->save();
+          break;
+        }
+      }
+    }
+  }
+}
+
+/**
  * Replaces the CKEditor to 5 from 4.
  */
 function govcms_post_update_replace_ckeditor5() {
