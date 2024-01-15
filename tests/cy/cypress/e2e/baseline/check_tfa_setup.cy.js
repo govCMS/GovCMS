7e5b42988bd9cc6e30c301eb7e950c7a35bcdb96
jackwrfuller <jack.fuller101@gmail.com>
2024-01-15 12:37:31 +1100

    Remove created keys automatically

--- tests/cy/cypress/e2e/baseline/check_tfa_setup.cy.js
+++ tests/cy/cypress/e2e/baseline/check_tfa_setup.cy.js
@@ -8,7 +8,7 @@ describe('Check TFA setup', () => {
   it('Create encryption key', () => {
     cy.visit('admin/config/system/keys')
     cy.visit('admin/config/system/keys/add')
-    cy.get('#edit-label').type("test2")
+    cy.get('#edit-label').type("test")
     // For some reason the site needs time here otherwise an error is thrown.
     // Something to do with the verification of #edit-label being machine-readable.
     cy.wait(500)
@@ -19,20 +19,14 @@ describe('Check TFA setup', () => {
     cy.get('[data-drupal-selector="edit-key-input-settings-key-value"]').type(Cypress.env("encryption_profile_key"))
     cy.get('[data-drupal-selector="edit-key-input-settings-base64-encoded"]').check()
     cy.get('#key-add-form').submit()
+    cy.get('.messages-list__item').contains('The key test has been added.')
   })
-
-
-
 
   it('Clean up', () => {
-    cy.visit('admin/config/system/keys')
-
-
-
-
-
-
+    // Remove created keys.
+    cy.visit('admin/config/system/keys/manage/test/delete?destination=/admin/config/system/keys')
+    cy.get('#edit-submit').click()
+    cy.get('.messages-list__item').contains('The key test has been deleted.')
   })
-
 
 })
