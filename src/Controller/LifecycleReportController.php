c6e5073c497598fabe0dd705ee074ff33aa7b048
Joseph Zhao <pandaski@outlook.com.au>
2024-06-14 11:43:36 +1000

    Theme the custom report page

--- src/Controller/LifecycleReportController.php
+++ src/Controller/LifecycleReportController.php
@@ -11,64 +11,115 @@ class LifecycleReportController extends ControllerBase {
   protected $lifecycle;
 
   // Create method is used for dependency injection.
+
+  public function __construct(Lifecycle $lifecycle) {
+    $this->lifecycle = $lifecycle;
+  }
+
+  // Constructor to inject the Lifecycle service.
+
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('govcms.lifecycle')
     );
   }
 
-  // Constructor to inject the Lifecycle service.
-  public function __construct(Lifecycle $lifecycle) {
-    $this->lifecycle = $lifecycle;
-  }
-
   // Method to display the lifecycle report.
+
   public function displayLifecycle() {
     $govcmsVersion = $this->getGovcmsVersion(); // Fetch the GovCMS version.
 
     $lifecycleData = $this->lifecycle->getLifecycleStatus();
-    $modulesTable = $this->formatDataForTable($lifecycleData['modules'], 'Modules');
-    $themesTable = $this->formatDataForTable($lifecycleData['themes'], 'Themes');
 
-    return [
+    $govcmsLifeCycleData = [];
+    foreach ($lifecycleData as $type => $statuses) {
+      foreach ($statuses as $status => $items) {
+        foreach ($items as $item) {
+          $govcmsLifeCycleData[] = [$item, ucfirst($status), ucfirst($type)];
+        }
+      }
+    }
+    $govcmsLifeCycleTable = $this->formatDataForTable($govcmsLifeCycleData);
+
+    $govcms_general_info_item = [
       [
         '#type' => 'markup',
-        '#markup' => '<h2>General System Information</h2>'
-      ],
-      [
-        '#type' => 'markup',
-        '#markup' => $this->t('GovCMS Version: @version', ['@version' => $govcmsVersion]),
-        '#prefix' => '<div>',
+        '#markup' => '<h3 class="system-status-general-info__item-title">GovCMS Version</h3>' . $this->t('@version', ['@version' => $govcmsVersion]),
+        '#prefix' => '<div class="system-status-general-info__item card">',
         '#suffix' => '</div>',
       ],
+    ];
+
+    $govcms_general_info_items = [
+      '#type' => 'markup',
+      '#markup' => '',
+      '#prefix' => '<div class="system-status-general-info__items">',
+      '#suffix' => '</div>',
+      'items' => $govcms_general_info_item,
+    ];
+
+    $govcms_general_info = [
       [
         '#type' => 'markup',
-        '#markup' => '<h2>Lifecycle</h2>'
+        '#markup' => '<h2 class="system-status-general-info__header">General System Information</h2>',
       ],
-      $modulesTable,
-      $themesTable,
+      $govcms_general_info_items,
+    ];
+
+    $govcms_general_info_render_array = [
+      '#type' => 'markup',
+      '#markup' => '',
+      '#prefix' => '<div class="system-status-general-info">',
+      '#suffix' => '</div>',
+      'items' => $govcms_general_info,
+    ];
+
+    return [
+      $govcms_general_info_render_array,
+      [
+        '#type' => 'markup',
+        '#markup' => '<h2 class="system-status-general-info__header">Lifecycle</h2>',
+      ],
+      $govcmsLifeCycleTable,
     ];
   }
 
   // Helper method to format data for the table.
-  private function formatDataForTable($data, $type) {
-    $rows = [];
-    foreach ($data as $status => $items) {
-      foreach ($items as $item) {
-        $rows[] = [$item, $status];
-      }
-    }
-    return [
-      '#theme' => 'table',
-      '#header' => [$type, 'Lifecycle', 'Status', 'EOL'],
-      '#rows' => $rows,
-      '#empty' => $this->t('No items to display.'),
-    ];
-  }
 
-  // Method to fetch the GovCMS version.
   private function getGovcmsVersion() {
     $profile = \Drupal::service('extension.list.profile')->get('govcms');
     return $profile->info['version'] ?? 'Unknown';
   }
+
+
+  private function formatDataForTable($data): array {
+    $rows = [];
+    foreach ($data as $item) {
+      $rows[] = [
+        'data' => [
+          ['data' => $item[0], 'class' => ['column-item']],
+          ['data' => $item[2], 'class' => ['column-type']],
+          ['data' => $item[1], 'class' => ['column-status']],
+          ['data' => 'EOL Placeholder', 'class' => ['column-eol']],
+        ],
+        'class' => ['align-middle'],
+      ];
+    }
+
+    return [
+      '#type' => 'table',
+      '#header' => [
+        ['data' => 'Name', 'class' => ['column-name']],
+        ['data' => 'Type', 'class' => ['column-type']],
+        ['data' => 'Status', 'class' => ['column-status']],
+        ['data' => 'EOL', 'class' => ['column-eol']],
+      ],
+      '#rows' => $rows,
+      '#empty' => t('No items to display.'),
+      '#attributes' => [
+        'class' => ['table', 'table-striped', 'table-bordered'],
+      ],
+    ];
+  }
+
 }
